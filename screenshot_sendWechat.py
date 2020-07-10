5e0ccefafb623b528a44b760287fd43eb87c7f78
LiuzJY <48585263+LiuzJY@users.noreply.github.com>
2020-07-10 17:46:36 +0800

    Update screenshot_sendWechat.py

--- screenshot_sendWechat.py
+++ screenshot_sendWechat.py
@@ -17,7 +17,7 @@ from selenium.webdriver.common.keys import Keys
 
 def login(dr):
     dr.maximize_window()
-    dr.get("http://10.25.150.103:19095/d/Kz4-4g6Wk/guan-jian-zhi-biao-zhan-shi-tu?orgId=1&kiosk=tv&from=now-6h&to=now")
+    dr.get("www.baidu.com")
 
     dr.find_element(By.NAME, 'user').send_keys("migu")
     dr.find_element(By.NAME, 'password').send_keys("migu")
@@ -44,7 +44,7 @@ def msg():
 
     locale.setlocale(locale.LC_CTYPE, 'chinese')    # 时间及日期以中文格式显示
     time_msg = late.strftime('%m月%d日 %H:%M' + '-' + now.strftime('%H:%M'))
-    msg_txt = "客户端后台系统 "+time_msg+" 系统运行正常"
+    msg_txt = "*****系统 "+time_msg+" 系统运行正常"
     return msg_txt
 
 
